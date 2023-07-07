85bf0b3e288fda5919af1a28b2835602212d6f81
DapperDataAnalyst <rww170030@gmail.com>
2023-07-07 11:42:11 -0500

    Complete

--- Practice Finding Important Properties of Matrices.py
+++ Practice Finding Important Properties of Matrices.py
@@ -83,3 +83,136 @@ goal = np.array([1,1])
 
 test = matrix @ goal == 1 * goal
 print(test)
+
+
+# Q4 --------------------------------------------------------------------------------------------------
+# Create a matrix
+matrix = np.array([[1,0], [0,2]])
+
+# Find eigenvalues and eigenvectors
+eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+# Print the eigenvalues
+print("Eigenvalues:")
+print(eigenvalues)
+
+# Create eigenvector choices from multichoice
+# For eigval 1:
+goal = np.array([1,0])
+
+test = matrix @ goal == 1 * goal
+print(test)
+
+# For eigval 1:
+goal = np.array([0,1])
+
+test = matrix @ goal == 1 * goal
+print(test)
+
+# For eigval 2:
+goal = np.array([0,1])
+
+test = matrix @ goal == 2 * goal
+print(test)
+
+
+# Q5 --------------------------------------------------------------------------------------------------
+# Create a matrix
+matrix = np.array([[1,0,0],
+                   [0,2,0],
+                   [0,0,3]])
+
+# Find eigenvalues and eigenvectors
+eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+# Print the eigenvalues
+print("Eigenvalues:")
+print(eigenvalues)
+
+# Create eigenvector choices from multichoice
+# For eigval 1:
+goal = np.array([1,0,0])
+
+test = matrix @ goal == 1 * goal
+print(test)
+
+# For eigval 2:
+goal = np.array([0,1,0])
+
+test = matrix @ goal == 2 * goal
+print(test)
+
+# For eigval 3:
+goal = np.array([0,0,1])
+
+test = matrix @ goal == 3 * goal
+print(test)
+
+
+# Q6 --------------------------------------------------------------------------------------------------
+seq = range(1,9)
+seq = list(seq)
+matrix = np.diag(seq)
+print(matrix)
+
+# Find eigenvalues and eigenvectors
+eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+# Print the eigenvalues
+print("Eigenvalues:")
+print(eigenvalues)
+
+# For eigval 1:
+goal = np.array([1,0,0,0,0,0,0,0])
+
+test = matrix @ goal == 1 * goal
+print(test)
+
+# For eigval 2:
+goal = np.array([0,1,0,0,0,0,0,0])
+
+test = matrix @ goal == 2 * goal
+print(test)
+
+# For eigval 8:
+goal = np.array([0,0,0,0,0,0,0,1])
+
+test = matrix @ goal == 8 * goal
+print(test)
+
+
+# Q7 --------------------------------------------------------------------------------------------------
+matrix = np.array([[1,2],
+                   [3,4]])
+
+trace = np.trace(matrix)
+print(trace)
+
+
+# Q8 --------------------------------------------------------------------------------------------------
+matrix = np.array([[1,263],
+                   [-342,2]])
+
+trace = np.trace(matrix)
+print(trace)
+
+
+# Q9-Q11 --------------------------------------------------------------------------------------------------
+matrix = np.array([[-4,0,0,0,0,0],
+                   [0,3,0,0,0,0],
+                   [0,0,5,0,0,0],
+                   [0,0,0,2,0,0],
+                   [0,0,0,0,-1,0],
+                   [0,0,0,0,0,3]])
+
+# Find the Frobenius norm of the matrix
+norm = np.linalg.norm(matrix, ord='fro')
+print(norm)
+
+# Find the operator norm of the matrix
+norm = np.linalg.norm(matrix, ord=2)
+print(norm)
+
+# Find the nuclear norm of the matrix
+norm = np.linalg.norm(matrix, ord='nuc')
+print(norm)
