81d75ef5d37b44ace4c8c9cb737918188cad88ba
DapperDataAnalyst <rww170030@gmail.com>
2023-07-07 17:40:59 -0500

    a

--- Staff-Graded Recap Matrices.py
+++ Staff-Graded Recap Matrices.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 
-# Q1 ============================================================================================
+# Part (a) ============================================================================================
 # Read the CSV file
 data = np.genfromtxt('TexasCityDistanceMatrix.csv', delimiter=',')
 
@@ -13,13 +13,52 @@ max_value = np.nanmax(matrix)
 print(max_value)
 
 
-# Q2 ============================================================================================
+# Part (b) ============================================================================================
+# Note that the row and column headers have been removed, and thus Katy is the 750th row in the matrix
+# in its current form
+
+# Find row of interest
+row_values = matrix[749, :]
+
+# Remove zeroes
+nonzero_values = row_values[row_values != 0]
+
+# Find mins
+min_nonzeroes = np.nanmin(nonzero_values)
+print(min_nonzeroes)
 
 
+# Find locations within 25 miles of Flower Mound
+# Flower Mound is the 507th row in the matrix
+# Find row of interest
+row_values = matrix[506, :]
+
+# Remove zeroes
+nonzero_values = row_values[row_values != 0]
+
+# Find values less than 25
+values = nonzero_values[nonzero_values <= 25]
+print(len(values))
 
 
+# Part (c) ============================================================================================
+start = time.time()
+np.sum(matrix)
+end = time.time()
+print(end - start)
 
 
+# Part (d) ============================================================================================
+# Set values greater than 50 to zero
+matrix[matrix > 50] = 0
+
+# Set NAs to zero
+matrix = np.nan_to_num(matrix)
+
+start = time.time()
+np.sum(matrix)
+end = time.time()
+print(end - start)
 
 
 
